def9460faa7cfe5b15123ee8f7696ffc214980cb
YuriiTrofimenko <tyaa@ukr.net>
2020-03-27 16:50:52 +0200

    init, demo and task 2

---
+++ src/org/tyaa/javaoop1/Group.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tyaa.javaoop1;
+
+/**
+ *
+ * @author yurii
+ */
+// Описание класса "Учебный класс"
+public class Group {
+    /* Описание аттрибутов класса */
+    // Поле класса Group - id типа int
+    public int id;
+    // Поле класса Group - number типа int
+    public int number;
+    // Поле класса Group - lit типа char
+    public char lit;
+}
